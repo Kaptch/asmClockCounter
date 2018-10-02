4dd572c93ff8dd558cbce1424de1886b5b69bf6d
Kaptch <kaptch@gmail.com>
2018-10-02 11:20:52 +0300

    test

--- test/test1.s
+++ test/test1.s
@@ -1,9 +1,11 @@
 .SECT .TEXT
         MOV     AX, (x)
         ADD     AX, (y)
-
+        MOV     SI, arr
+        add     AX, (SI)
 .SECT .DATA
 x:     .WORD    2
 y:     .WORD    3
-
+arr:   .WORD    2, 3, 4
+end:   .WORD    0
 .SECT .BSS
