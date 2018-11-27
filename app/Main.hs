f7b7220cbfa7d1292d2906cec8262fc45302b5fd
Kaptch <kaptch@gmail.com>
2018-11-27 12:18:13 +0300

    verbose output

--- app/Main.hs
+++ app/Main.hs
@@ -4,15 +4,11 @@ import Lib
 import System.Environment
 
 usage :: IO ()
-usage = do
-    putStrLn "<filename> [options]"
-    putStrLn "options: -v, --verbose"
+usage = putStrLn "<filename>"
 
 main :: IO ()
 main = do
     fn <- getArgs
     case fn of
-        [fn] -> print <$> countClocksFromParsedFile fn *> return ()
-        [fn, "-v"] -> runVerbose fn
-        [fn, "--verbose"] -> runVerbose fn
+        [fn] -> runVerbose fn
         _ -> usage
